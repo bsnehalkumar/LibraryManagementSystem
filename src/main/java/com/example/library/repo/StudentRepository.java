b0452e91a719499f90e0c975c2fa3fa302e8bd13
bsnehalkumar <98573022+bsnehalkumar@users.noreply.github.com>
2022-03-27 18:34:08 +0530

    Delete src/main directory

--- src/main/java/com/example/library/repo/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.library.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.library.entities.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-
-}
